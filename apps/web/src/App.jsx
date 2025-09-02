b5eec0b416bcb4d0e3db152cf88445581a8651f3
Lee Hitchcock <lee.hitchcock2041@gmail.com>
2025-09-02 16:19:17 +0100

    feat: integrate Tailwind CSS into the web application
    - Added Tailwind CSS dependencies to the web app's package.json.
    - Updated App component to use a Tailwind CSS class for styling.
    - Imported Tailwind CSS configuration in index.css.
    - Created a new Tailwind CSS configuration package with shared styles and PostCSS configuration.
    - Configured PostCSS to use Tailwind CSS in the new configuration package.
    - Updated design system package to include Tailwind CSS configuration as a dependency.

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-mint-500'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
