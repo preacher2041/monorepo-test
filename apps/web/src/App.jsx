c992c81ca614671397175f50b3ca6ac2c01b21a7
Lee Hitchcock <lee.hitchcock2041@gmail.com>
2025-09-02 15:29:05 +0100

    feat: add design system package with Counter component and integrate into web app

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import {Counter} from '@monorepo-test/design-system';
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -18,9 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Counter />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
