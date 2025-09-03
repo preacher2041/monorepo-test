0306ff9722719ecfc5a879b83bf945eaa1eb0455
Lee Hitchcock <lee.hitchcock2041@gmail.com>
2025-09-03 14:27:19 +0100

    Formatting

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,32 +1,36 @@
-import {Counter} from '@monorepo-test/design-system';
+import { Counter } from '@monorepo-test/design-system'
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <div className='bg-mint-500'>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Counter />
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div className='bg-mint-500'>
+				<a href='https://vite.dev' target='_blank' rel='noreferrer'>
+					<img src={viteLogo} className='logo' alt='Vite logo' />
+				</a>
+				<a href='https://react.dev' target='_blank' rel='noreferrer'>
+					<img
+						src={reactLogo}
+						className='logo react'
+						alt='React logo'
+					/>
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className='card'>
+				<Counter />
+				<p>
+					Edit <code>src/App.jsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className='read-the-docs'>
+				Click on the Vite and React logos to learn more
+			</p>
+		</>
+	)
 }
 
 export default App
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.js'
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('root')
 if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
+	createRoot(rootElement).render(
+		<StrictMode>
+			<App />
+		</StrictMode>
+	)
 }
